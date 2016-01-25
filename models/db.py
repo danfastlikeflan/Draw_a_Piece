523e5ccb61cc8dc060053452fdb9c6a4d917252c
Raman Nakarmi <rnakarmi@ucsc.edu>
2016-01-25 12:49:06 -0800

    fixed database setup

--- models/db.py
+++ models/db.py
@@ -1,41 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-## File is released under public domain and you can use without limitations
-#########################################################################
-
-## if SSL/HTTPS is properly configured and you want all HTTP requests to
-## be redirected to HTTPS, uncomment the line below:
-# request.requires_https()
-
-## app configuration made easy. Look inside private/appconfig.ini
-from gluon.contrib.appconfig import AppConfig
-## once in production, remove reload=True to gain full speed
-myconf = AppConfig(reload=True)
-
-
-if not request.env.web2py_runtime_gae:
-    ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
-else:
-    ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore+ndb')
-    ## store sessions and tickets there
-    session.connect(request, response, db=db)
-    ## or store session in Memcache, Redis, etc.
-    ## from gluon.contrib.memdb import MEMDB
-    ## from google.appengine.api.memcache import Client
-    ## session.connect(request, response, db = MEMDB(Client()))
-
-## by default give a view/generic.extension to all actions from localhost
-## none otherwise. a pattern can be 'controller/function.extension'
-response.generic_patterns = ['*'] if request.is_local else []
-## choose a style for forms
-response.formstyle = myconf.take('forms.formstyle')  # or 'bootstrap3_stacked' or 'bootstrap2' or other
-response.form_label_separator = myconf.take('forms.separator')
-
-
 ## (optional) optimize handling of static files
 # response.optimize_css = 'concat,minify,inline'
 # response.optimize_js = 'concat,minify,inline'
@@ -50,8 +14,34 @@ response.form_label_separator = myconf.take('forms.separator')
 ## - old style crud actions
 ## (more options discussed in gluon/tools.py)
 #########################################################################
+db = DAL('sqlite://drawAPiece.db')
+db.define_table('pictures',
+	Field('picture', type='upload'),
+	Field('num','integer'),
+	Field('finished','boolean')
+)
+
+db.define_table('project',
+	Field('name', 'string'),
+	Field('id','integer'),
+	Field('pictureId','integer'),
+	Field('isPublic','boolean'),
+	Field('width','integer'),
+	Field('height','integer')
+)
+
+db.define_table('userName',
+	Field('name', 'string'),
+	Field('password','string'),
+	Field('projectId','integer')
+)
+
+from gluon.contrib.appconfig import AppConfig
+
+myconf = AppConfig(reload=True)
 
 from gluon.tools import Auth, Service, PluginManager
+
 
 auth = Auth(db)
 service = Service()
@@ -91,23 +81,3 @@ auth.settings.reset_password_requires_verification = True
 ## after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
 
-db.define_table('pictures',
-    Field('picture', type='upload'),
-    Field('num','integer'),
-    Field('finished','boolean')
-)
-
-db.define_table('project',
-    Field('name', 'string'),
-    Field('id','integer'),
-    Field('pictureId','integer'),
-    Field('isPublic','boolean'),
-    Field('width','integer'),
-    Field('height','integer')
-)
-
-db.define_table('userName',
-    Field('name', 'string'),
-    Field('password','string'),
-    Field('projectId','integer')
-)
